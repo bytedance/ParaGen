7fdf1918faba6221ade0cf9ca246d4e437e9772a
JiangtaoFeng <fjt159875321>
2022-04-16 14:34:43 +0800

    fix preprocess

--- paragen/entries/preprocess.py
+++ paragen/entries/preprocess.py
@@ -1,10 +1,13 @@
 from paragen.entries.util import parse_config
 from paragen.tasks import create_task, AbstractTask
 from paragen.datasets import create_dataset, AbstractDataset
+from paragen.utils.runtime import build_env
 
 
 def main():
     confs = parse_config()
+    if 'env' in confs:
+        build_env(confs['task'], **confs['env'])
     task = create_task(confs.pop('task'))
     assert isinstance(task, AbstractTask)
     task.build()
