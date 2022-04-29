9b3c0210dc220cf8903b6df566cc659224c4f5ff
JiangtaoFeng <fjt159875321>
2022-04-29 14:59:09 +0800

    fix create

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -5,20 +5,20 @@ from paragen.utils.data import possible_eval
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--hypo', type=str, help='hypothesis')
-parser.add_argument('--ref', default='', type=str, help='ground truth reference')
-parser.add_argument('--metric', default=None, type=str, help='metric class name')
+parser.add_argument('--ref', type=str, help='ground truth reference')
+parser.add_argument('--metric', type=str, help='metric class name')
 args, unknown = parser.parse_known_args()
 
-kwargs = {}
+conf = {'class': args.metric}
 current_key = None
 for ele in unknown:
     if ele.startswith("--"):
         current_key = ele[2:]
     elif current_key is not None:
-        kwargs[current_key] = possible_eval(ele)
+        conf[current_key] = possible_eval(ele)
         current_key = None
 
-metric = create_metric(args.metric, **kwargs)
+metric = create_metric(conf)
 metric.build()
 
 with open(args.hypo, 'r ') as fhypo, open(args.ref, 'r') as fref:
